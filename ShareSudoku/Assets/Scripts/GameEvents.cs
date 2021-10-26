6886299bc84f481568479c03b3f99e05c19bf6cc
emv1300 <82583723+emv1300@users.noreply.github.com>
2021-10-26 18:34:34 +0300

    created a script for handling the event for adding a number to the grid

---
+++ ShareSudoku/Assets/Scripts/GameEvents.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public delegate void UpdateSqNum(int number);
+    
+    public static event UpdateSqNum OnUpdateSqNum;
+
+    public static void UpdateSqNumMethod(int number)
+    {
+        if (OnUpdateSqNum != null)
+            OnUpdateSqNum(number);
+    }
+
+    public delegate void SelectedSquare(int squareIndex);
+
+    public static event SelectedSquare UpdateSelectedSquare;
+
+    public static void SquareSelectedMethod(int squareIndex)
+    {
+        if (UpdateSelectedSquare != null)
+            UpdateSelectedSquare(squareIndex);
+    }
+
+}
