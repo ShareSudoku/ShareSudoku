8c74d2c1fb5b90efa84c0ee8bb163b1a2ea0c2a9
emv1300 <82583723+emv1300@users.noreply.github.com>
2021-10-28 19:33:07 +0300

    added script for dealing with player lives

---
+++ ShareSudoku/Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour
+{
+
+    public List<GameObject> errorXs;
+
+    private int lives = 0;
+    private int errorNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = errorXs.Count;
+        errorNum = 0;
+    }
+
+    private void WrongNumber()
+    {
+        if(errorNum < errorXs.Count)
+        {
+            errorXs[errorNum].SetActive(true);
+            errorNum++;
+            lives--;
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.OnWrongNumber += WrongNumber;   
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnWrongNumber -= WrongNumber;
+    }
+}
